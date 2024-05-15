295b2d62114f7e196de23d67209e6f1eac1f1731
Pedro Galan <pedrogalanagu1@gmail.com>
2024-05-15 20:18:43 +0200

    Modificacion en el estilo login

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
 <title>Registrar usuario</title>
 <meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="estilo.css">
+<link rel="stylesheet" type="text/css" href="estilologin.css">
 </head>
 <body>
     <form method="post">
