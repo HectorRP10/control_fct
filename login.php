ae76162cb4c5708d4d21a743dc692f9a5c2b056f
Pedro Galan <pedrogalanagu1@gmail.com>
2024-05-15 20:05:10 +0200

    T1 Control Básico

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Registrar usuario</title>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+    <form method="post">
+    <img src="logo__.svg" alt="Logo mal">
+    <h1>Inicio de Sesión</h1>
+    <input type="text" name="usuario" placeholder="Nombre">
+    <input type="password" name="password" placeholder="Contraseña">
+    <input type="submit" name="enviar" value="Iniciar Sesion">
+    </form>
+
+      
+</body> 
+</html>
