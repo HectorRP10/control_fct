ea4a1b72a8dc544d2672474f3357343a444d806e
Pedro Galan <pedrogalanagu1@gmail.com>
2024-05-16 02:08:24 +0200

    Links Bootstrap

--- alumnoPrioridad.php
+++ alumnoPrioridad.php
@@ -5,9 +5,10 @@
     <title>Dashboard_alumno</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" type="text/css" href="alumnonav.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<header>
+    <header>
         <img class="logo" src="https://aulavirtual.elcampico.org/pluginfile.php/1/core_admin/logocompact/300x300/1711458408/logo__.png" alt="El Campico Logo"> 
         <nav class="nav_header" >
             <ul class="ul_header">
@@ -19,6 +20,6 @@
         </nav>
     </header>
     
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
